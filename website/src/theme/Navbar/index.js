ced1d514a023438c34bf9e39e48dc80943f76233
Olivier Poupeney <olivier.poupeney@icloud.com>
2025-11-06 12:34:13 -0500

    New event banner component part of the navbar component

--- website/src/theme/Navbar/index.js
+++ website/src/theme/Navbar/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavbarLayout from "@theme/Navbar/Layout";
+import NavbarEventBanner from "@theme/Navbar/EventBanner";
 import NavbarContent from "@theme/Navbar/Content";
 export default function Navbar() {
   return (
     <NavbarLayout>
+      <NavbarEventBanner />
       <NavbarContent />
     </NavbarLayout>
   );
